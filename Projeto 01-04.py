8a7961fdec2d7cdbb4af2762c776a9ef4d3931ef
Mário Yama <101875882+MarioYama7@users.noreply.github.com>
2022-07-27 13:42:52 -0300

    27-07 Classifica objetos
    Classifique dois objetos materiais e dois abstratos. Insira, no mínimo, três métodos e três atributos para cada.

---
+++ Projeto 01-04.py
@@ -0,0 +1,109 @@
+# Indicar métodos e atributos em 4 objetos
+
+# Objetos Materiais:
+
+# Objeto 01:
+# Atributos do 01:
+
+class caneta:
+    def __init__(self, marca, material, cor):
+        self.marca = marca
+        self.material = material
+        self.cor = cor
+
+# Métodos do 01:
+
+
+def escrever(self):
+    print('A caneta escreve')
+
+
+def coçar_cabeça(self):
+    print('Pode coçar a cabeça com a caneta. Rs')
+
+
+def recarregar(self)
+
+
+print('A caneta pode ser recarregada')
+
+# Objeto 02:
+# Atributos do 02:
+
+
+class Smartphone:
+    def __init__(self, ano_lancamento, modelo, peso):
+        self.ano_lancamento = ano_lancamento
+        self.modelo = modelo
+        self.peso = peso
+
+# Métodos do 02:
+
+
+def ligar(self):
+    print('O Smartphone liga')
+
+
+def ligacoes(self):
+    print('No smartphone é possível fazer ligações')
+
+
+def tirar_foto(self)
+
+
+print('No smartphone é possível tirar fotos')
+
+# ------Objetos Abstratos------
+
+# Objeto 01:
+# Atributos do 01:
+
+
+class planilha:
+    def __init__(self, linhas, colunas, celulas):
+        self.linhas = linhas
+        self.colunas = colunas
+        self.celulas = celulas
+
+# Métodos do 01:
+
+
+def inserir_numero(self):
+    print('Insercao de numeros')
+
+
+def permitir_graficos(self):
+    print('Permicao de graficos')
+
+
+def calculos(self)
+
+
+print('Permissao de calculos')
+
+# Objeto 02:
+
+# Atributos do 02:
+
+
+class acoes:
+    def __init__(self, preco_de_compra, preco_de_venda, dividendos):
+        self.preco_de_compra = preco_de_compra
+        self.preco_de_venda = preco_de_venda
+        self.dividendos = dividendos
+
+# Métodos do 02:
+
+
+def comprar_acao(self):
+    print('Permitir compra de acao')
+
+
+def vender_acao(self):
+    print('Permitir venda de acao')
+
+
+def receber_dividendos(self)
+
+
+print('Permitir recebimento de dividendos de acao')
