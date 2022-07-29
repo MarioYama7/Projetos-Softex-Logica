1a4f63a2583a59f9743bb513a9f876bf409d71e1
Mário Yama <101875882+MarioYama7@users.noreply.github.com>
2022-07-28 21:50:39 -0300

    28-07
    Crie uma classe de sua preferência com, no mínimo, uma variável, um método e um incremento. Depois, desenvolva três ou mais objetos para testar o código.

---
+++ Projeto 02-04.py
@@ -0,0 +1,29 @@
+# Incremento em python com apostas
+
+class apostadas:
+    def __init__(self, n1, n2, n3, n4, n5):
+        self.n1 = n1
+        self.n2 = n2
+        self.n3 = n3
+        self.n4 = n4
+        self.n5 = n5
+
+
+class sorteadas:
+    def __init__(self, s1, s2, s3, s4, s5):
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+        self.s4 = s4
+        self.s5 = s5
+
+
+def sorteio(self, value):
+    for apostada in apostadas:
+        for sorteada in sorteadas:
+            if apostada == sorteada:
+                self.sorteio += 1
+            break
+
+
+print(sorteio)
